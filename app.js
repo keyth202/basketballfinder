e13f9fd941479f27792db1d22fa49fefe92ce342
keyth202 <combs.cdi@gmail.com>
2017-04-11 18:55:26 -0400

    After both meetup and google maps work

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ $('#js-search-form').submit(function(event){
 		// Add in Case if query is not 5 letters long
 		console.log(result);
 		searchMeetup(result, displayMeetup);
+		//locationMeetup();
+
 
 	});
 });
@@ -43,18 +45,57 @@ function searchMeetup(search, callback){
 function displayMeetup(data){
 	 var searchResults = data.data;
 	 console.log(data);
-	$('.js-results').html('<h2>Here are the Top 10 Groups in your area</h2>');
+	$('.js-results').html('<h2 class="title">Here are the Top 10 Groups in your area</h2>');
 	console.log(searchResults.length);
 	console.log(data.data.length);
 	for(var i = 0; i<searchResults.length; i++){
-			$('.js-results').append('<p>'+searchResults[i].name);
+			$('.js-results').append('<div class="meetup" id="'+i+'"><h2>'+searchResults[i].name+'</h2><p>'+searchResults[i].description+'</p><p>'+searchResults[i].city+'</p>'+
+				"<input type='hidden' value='"+searchResults[i].lon+"' class='longitude'><input type='hidden' value='"+searchResults[i].lat+"' class='latitude'></div>");
+
 	}
+	/* Set up onclick statement for picture and name 
+	*/
+	
 } 
+$('.js-results').on('click','.meetup', function(e){
+		console.log($(this));
+		console.log($(this).children());
+		console.log($(this).find('.longitude').val());
+		console.log($(this).find('.latitude').val());
+		var lng1= $(this).find('.longitude').val();
+		var lat1= $(this).find('.latitude').val();
+		placepointGoogle(lng1,lat1);
+	});
 
-function searchGoogle(search, callback){
+function placepointGoogle(longitude,latitude){
 
+	if(longitude && latitude){
+		$('#map').removeClass('hide');
+			var uluru = {lat: Number(latitude), lng:Number(longitude)};
+			var map = new google.maps.Map(document.getElementById('map'),{
+			zoom: 10,
+			center:uluru
+		});
+		var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+	}
+		
+		
 }
+//	initMap(longitude,latitude);
 
-function displayGoogle(data){
-
-}
+/*function initMap(longitude, latitude) { 
+  // var latitude = ;
+   // var longitude = ;
+    var location = {lat: Number(latitude), lng: Number(longitude)};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 16,
+      center: location
+    });
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+}*/
